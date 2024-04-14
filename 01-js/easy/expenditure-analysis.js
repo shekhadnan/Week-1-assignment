ff661f54050e95734bc35ccfbbda51dec792c591
shekhadnan <31979369+shekhadnan@users.noreply.github.com>
2024-04-14 20:29:25 +0530

    Update expenditure-analysis.js

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -9,7 +9,40 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  var obj = {};
+  var arr = [];
+  for (var i = 0; i < transactions.length; i++) {
+    if (!obj[transactions[i].category]) {
+      obj[transactions[i].category] = 0;
+    }
+    obj[transactions[i].category] =
+      obj[transactions[i].category] + transactions[i].price;
+  }
+  for (let category in obj) {
+    arr.push({ [category]: obj[category] });
+  }
+  return arr;
+}
+
+const transactions = [
+  { itemName: "Item 1", category: "Food", price: 20, timestamp: "2024-04-14" },
+  { itemName: "Item 2", category: "Food", price: 30, timestamp: "2024-04-14" },
+  {
+    itemName: "Item 3",
+    category: "Clothing",
+    price: 50,
+    timestamp: "2024-04-14",
+  },
+  {
+    itemName: "Item 4",
+    category: "Electronics",
+    price: 100,
+    timestamp: "2024-04-14",
+  },
+  { itemName: "Item 5", category: "Food", price: 25, timestamp: "2024-04-14" },
+];
+
+console.log(calculateTotalSpentByCategory(transactions));
 }
 
 module.exports = calculateTotalSpentByCategory;
